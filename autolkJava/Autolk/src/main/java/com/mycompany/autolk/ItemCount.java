f6cf327cf941bf1ded730d7819f060fc2d175cca
Tharaka Ariyarathna <69495064+tharaka-ariyarathna@users.noreply.github.com>
2022-02-21 07:43:04 +0530

    Changed ItemCount class

--- autolkJava/Autolk/src/main/java/com/mycompany/autolk/ItemCount.java
+++ autolkJava/Autolk/src/main/java/com/mycompany/autolk/ItemCount.java
@@ -4,9 +4,12 @@ import javax.swing.*;
 import java.sql.SQLException;
 
 public class ItemCount {
-    public void displayCount(JFrame frame, DataBase dataBase, JPanel panel ) throws SQLException {
+    public void displayCount(DataBase dataBase, JPanel panel ) throws SQLException {
         String query = "SELECT COUNT(*) FROM items" ; // Assuming table name is items
         int totalItems = dataBase.getData(query) ;
         JLabel totalItemsLabel = new JLabel(String.valueOf(totalItems)) ;
+        //Change the size accordingly
+        totalItemsLabel.setBounds(10, 200, 20, 25);
+        panel.add(totalItemsLabel) ;
     }
 }
